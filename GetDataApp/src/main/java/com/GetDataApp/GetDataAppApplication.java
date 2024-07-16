1c54fd99b0491b22200e648a616642bcd53cc30d
DELL <DELL@DESKTOP-TL6FBHF>
2024-07-16 22:35:22 +0530

    commit  sky

---
+++ GetDataApp/src/main/java/com/GetDataApp/GetDataAppApplication.java
@@ -0,0 +1,13 @@
+package com.GetDataApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetDataAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetDataAppApplication.class, args);
+	}
+
+}
---
+++ GetDataApp/src/test/java/com/GetDataApp/GetDataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GetDataApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetDataAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
